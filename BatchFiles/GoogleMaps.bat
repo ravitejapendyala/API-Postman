0eb2f08e061a4c930141d8651786d245e97882e5
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-06-12 23:04:12 +0530

    Update postman collection --adding folder

--- BatchFiles/GoogleMaps.bat
+++ BatchFiles/GoogleMaps.bat
@@ -1,3 +1,3 @@
 cd C:\Users\1021285\OneDrive - Blue Yonder\Personal\JavaProjects\API-Postman
 echo %~n0
-newman run --insecure -e "EnvData\GoogleMaps.postman_environment.json" -r htmlextra --reporter-htmlextra-export "Reports\GoogleMapsAPI.html" --reporter-htmlextra-title "Google Maps API" -d "TestData\System\Access Rules\Access Rules Get.csv" "PostmanTests\Collections\System\Access Rules\Access Rules.postman_collection.json" --folder "Get Access Rules"
+newman run --insecure -e "EnvData\GoogleMaps.postman_environment.json" -r htmlextra --reporter-htmlextra-export "Reports\GoogleMapsAPI.html" --reporter-htmlextra-title "Google Maps API" -d "TestData\GoogleAPI\MapsLanguage.csv" "PostmanTests\Google Maps.postman_collection.json" --folder "Regression"
